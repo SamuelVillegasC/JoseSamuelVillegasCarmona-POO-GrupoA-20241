9db215d8db5256a5d4ad774f2640278a17d75092
SamuelVillegasC <159852053+SamuelVillegasC@users.noreply.github.com>
2024-02-27 22:38:59 -0600

    Actualizar rectangulo.java
    finalizado el código del ejercicio 1

--- rectangulo.java
+++ rectangulo.java
@@ -16,3 +16,17 @@ public class Rectangulo {
         return base * altura;
     }
 }
+
+public class Hw5 {
+    public static void main(String[] args) {
+
+  System.out.println("\t\tClase Rectangulo: ");
+      Rectangulo rec1 = new Rectangulo();
+     System.out.println("Area enteros: "+rec1.area(11, 2));
+        System.out.println("Perimetro enteros: "+rec1.perimetro(11, 2));
+
+        Rectangulo rec2 = new Rectangulo();
+        System.out.println("Area doubles: "+rec2.area(9.54, 8.13));
+        System.out.println("Perimetro doubles: "+rec2.perimetro(9.54, 8.13));
+}
+}
