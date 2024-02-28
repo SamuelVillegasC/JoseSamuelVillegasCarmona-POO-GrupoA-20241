23c50acc8bd19a638fb57ca66ba1c50765492977
SamuelVillegasC <159852053+SamuelVillegasC@users.noreply.github.com>
2024-02-27 22:32:57 -0600

    Actualizar rectangulo.java
    clase y metodos rectangulo

--- rectangulo.java
+++ rectangulo.java
@@ -1,3 +1,18 @@
-public class main{
+public class Rectangulo {
 
+    public double perimetro(double base, double altura){
+        return 2*(base + altura);
+    }
+
+    public int perimetro(int base, int altura){
+        return 2*(base + altura);
+    }
+
+    public double area(double base, double altura){
+        return base * altura;
+    }
+
+    public int area(int base, int altura){
+        return base * altura;
+    }
 }
